f5baa2d7b8ee14d8b008c8b8a03d3a7989323961
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-31 01:17:57 +0300

    Add CRUD for advertisements

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -8,4 +8,42 @@ class AdvertisementsController < ApplicationController
   def show
     @advertisement = Advertisement.find(params[:id])
   end
+
+  def new
+    @advertisement = current_user.advertisements.new
+  end
+
+  def create
+    @advertisement = current_user.advertisements.new(advertisement_params)
+
+    @advertisement.status = params[:advertisement][:draft] == '1' ? :draft : :pending_review
+
+    if @advertisement.save
+      redirect_to advertisement_path(@advertisement.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def update
+    @advertisement = current_user.advertisements.find(params[:id])
+
+    @advertisement.status = params[:advertisement][:draft] == '1' ? :draft : :pending_review
+
+    if @advertisement.update(advertisement_params)
+      redirect_to advertisement_path(@advertisement.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def advertisement_params
+    params.require(:advertisement).permit(:title, :description)
+  end
 end
