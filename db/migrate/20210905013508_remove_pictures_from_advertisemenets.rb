0fdf489cb46fa888817463a48d918af919ad892f
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-09-05 04:42:45 +0300

    Make remove pictures column migration reversible

--- db/migrate/20210905013508_remove_pictures_from_advertisemenets.rb
+++ db/migrate/20210905013508_remove_pictures_from_advertisemenets.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class RemovePicturesFromAdvertisemenets < ActiveRecord::Migration[6.1]
   def change
-    remove_column :advertisements, :pictures
+    remove_column :advertisements, :pictures, :json
   end
 end
