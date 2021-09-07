615d983ca38d60817ee72aaf95e3977776627f36
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-09-07 14:57:50 +0300

    Destroy unused migration

--- db/migrate/20210905013508_remove_pictures_from_advertisemenets.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemovePicturesFromAdvertisemenets < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :advertisements, :pictures, :json
-  end
-end
